75246e1bb3cd9fab8e8e05208675f9102d538d3a
Diego Garcia <diego350z1@gmail.com>
2023-08-20 19:02:50 -0600

    partials and google tags

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es" data-theme="light">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>App Name - @yield('title')</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    {{-- Google Tags --}}
+    @include('layouts.partials.google-tags')
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans antialiased">
+    <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+        @section('sidebar')
+            @include('partials.nav')
+        @show
+
+        <main>
+            @yield('content')
+        </main>
+    </div>
+</body>
+
+</html>
---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,21 @@
+<div class="navbar bg-base-100">
+    <div class="flex-1">
+        <a class="text-xl normal-case btn btn-ghost">daisyUI</a>
+    </div>
+    <div class="flex-none">
+        <ul class="px-1 menu menu-horizontal">
+            <li><a>Link</a></li>
+            <li>
+                <details>
+                    <summary>
+                        Parent
+                    </summary>
+                    <ul class="p-2 bg-base-100">
+                        <li><a>Link 1</a></li>
+                        <li><a>Link 2</a></li>
+                    </ul>
+                </details>
+            </li>
+        </ul>
+    </div>
+</div>
